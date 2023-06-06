1e56e8a127799092b0215d39137707337ac5fd4f
kevin <kevin19960908@gmail.com>
2023-06-06 16:04:07 +0800

    優化一半

--- 開啟伺服器.js
+++ 開啟伺服器.js
@@ -2,6 +2,15 @@
 var express = require('express');
 //開起伺服器
 var app = express();
+//建立時間
+var date = new Date();
+var month = date.getMonth()+1+'月';
+var day = date.getDay()+'日';
+var hour = date.getHours()+'時';
+var minutes = date.getMinutes()+'分';
+var seconds = date.getSeconds()+'秒';
+var time = month+day+hour+minutes+seconds;
+//建立結束
 //
 var bp = require('body-parser');
 var bodyParser = require('body-parser')//小心不要打錯字
@@ -11,5 +20,5 @@ app.get('/',function(req,res){
     res.send('pipipipipipipi')
 })
 app.listen(3000,function(){
-    console.log("pokemon get daze")
+    console.log(time+"pokemon get daze")
 })
