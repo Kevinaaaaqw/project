056ee164600de56ce51c231a748204d2727892d2
kevin <kevin19960908@gmail.com>
2023-06-06 15:08:30 +0800

    測試修改

--- 開啟伺服器.js
+++ 開啟伺服器.js
@@ -1,26 +1,10 @@
+//接收開啟伺服器功能
 var express = require('express');
+//開起伺服器
 var app = express();
-
-
+//
 var bp = require('body-parser');
-// {
-//     unescapeBuffer: [Function: unescapeBuffer],
-//     unescape: [Function: qsUnescape],
-//     escape: [Function: qsEscape],
-//     stringify: [Function: stringify],
-//     encode: [Function: stringify],
-//     parse: [Function: parse],
-//     decode: [Function: parse]
-//   }
-
-
 var bodyParser = require('body-parser')//小心不要打錯字
-// [Function (anonymous)] {
-//     json: [Getter],
-//     raw: [Getter],
-//     text: [Getter],
-//     urlencoded: [Getter]
-//   }
 app.set('view engine','ejs')
 app.get('/',function(req,res){
     console.log('pikachu~~~~');
