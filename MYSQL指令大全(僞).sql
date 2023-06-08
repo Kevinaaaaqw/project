2ade88c7e3b6deddd107287a1494e58fac2410eb
kevin <kevin19960908@gmail.com>
2023-06-08 16:52:20 +0800

    fuck

--- MYSQL指令大全(僞).sql
+++ MYSQL指令大全(僞).sql
@@ -1,8 +1,14 @@
---新增    進   資料表名  欄位名稱    欄位名稱   值         
+  --新增    進   資料表名  欄位名稱    欄位名稱   值         
   insert into pokemon (pokename, pokeimage) values ('妙蛙種子', '../image/pokemon/001.png');
+  
+  
   --查詢
   select * from 資料表名稱 where 欄位 = 名子
   --查詢                  的第幾筆找幾筆資料
   select * from 資料表名稱 limit 0 , 3;
   select * from 資料表名稱 limit 3 , 3;--範例
   select * from 資料表名稱 limit 6 , 3;--範例
+
+
+--計算某資料表內資料比數數量
+select count(*) from 資料表名稱
