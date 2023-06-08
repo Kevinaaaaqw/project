c087d48631da159dc8598d82baec3ed3fa4c25b7
kevin <kevin19960908@gmail.com>
2023-06-08 16:52:34 +0800

    mm

--- 開啟伺服器.js
+++ 開啟伺服器.js
@@ -1,10 +1,14 @@
-//接收開啟伺服器功能
-var express = require('express');
-//開起伺服器
-var app = express();
+var express = require('express');//接收開啟伺服器功能
+var app = express();//開起伺服器
+var bodyParser = require('body-parser');//小心不要打錯字
+var urlencoded = bodyParser.urlencoded()//解碼功能
 
-
-//建立時間
+app.set('view engine','ejs')
+app.get('/',function(req,res){
+    console.log('確定後台成功(要刪掉)');
+    res.send('建立成功(動工~你很棒)')
+})
+//伺服器建立時間
 var date = new Date();
 var month = date.getMonth()+1+'月';
 var day = date.getDay()+'日';
@@ -13,16 +17,7 @@ var minutes = date.getMinutes()+'分';
 var seconds = date.getSeconds()+'秒';
 var time = month+day+hour+minutes+seconds;
 //建立結束
-//
-
-
-var bp = require('body-parser');
-var bodyParser = require('body-parser')//小心不要打錯字
-app.set('view engine','ejs')
-app.get('/',function(req,res){
-    console.log('pikachu~~~~');
-    res.send('pipipipipipipi')
-})
 app.listen(3000,function(){
+    console.clear()
     console.log(time+"pokemon get daze")
 })
