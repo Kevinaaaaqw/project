f35deac61a7a499c81b7f3f6e7c7e5b8b2204833
kevin <kevin19960908@gmail.com>
2023-06-16 17:10:34 +0800

    ooooooo

--- routes/cart.js
+++ routes/cart.js
@@ -1,8 +1,66 @@
 var express = require('express')
 var page = express.Router()
 page.get('/',function(req,res){
+    let days = new Date("2023/04/11")
+    let daye = new Date("2023/04/15")
     res.render('cart/cart',{
-        product : "kevin"
+        item:[
+            {
+            productUser : "kevin",
+            product :[
+                {
+                    src : "public/img/product/coffee006冰滴咖啡去背.png",
+                    productName : "咖啡機",
+                    rentStart : "2023/04/11",
+                    rentEnd : "2023/04/15",
+                    day : daye.getDate()-days.getDate(),
+                    rent : "450",
+                    deposit : "1000"
+                },
+                {
+                    src : "public/img/product/coffee006冰滴咖啡去背.png",
+                    productName : "咖啡機",
+                    rentStart : "2023/04/11",
+                    rentEnd : "2023/04/15",
+                    day : daye.getDate()-days.getDate(),
+                    rent : "450",
+                    deposit : "1000"
+                },
+                {
+                    src : "public/img/product/coffee006冰滴咖啡去背.png",
+                    productName : "咖啡機",
+                    rentStart : "2023/04/11",
+                    rentEnd : "2023/04/15",
+                    day : daye.getDate()-days.getDate(),
+                    rent : "450",
+                    deposit : "1000"
+                },
+            ]
+        },
+            {
+            productUser : "kevin",
+            product :[
+                {
+                    src : "public/img/product/coffee006冰滴咖啡去背.png",
+                    productName : "咖啡機",
+                    rentStart : "2023/04/11",
+                    rentEnd : "2023/04/15",
+                    day : daye.getDate()-days.getDate(),
+                    rent : "450",
+                    deposit : "1000"
+                },
+                {
+                    src : "public/img/product/coffee006冰滴咖啡去背.png",
+                    productName : "咖啡機",
+                    rentStart : "2023/04/11",
+                    rentEnd : "2023/04/15",
+                    day : daye.getDate()-days.getDate(),
+                    rent : "450",
+                    deposit : "1000"
+                },
+            ]
+        },
+        ]
     })
 })
 page.get('/order',function(req,res){
